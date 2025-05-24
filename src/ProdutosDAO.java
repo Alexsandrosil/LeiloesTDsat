ec6519a5177cf3a3facefdb1314b3b7159984b81
Alexsandro <alexsandrosil16.contato@gmail.com>
2025-05-24 16:04:10 -0300

    Adicionado botão de vender

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -18,21 +18,61 @@ public class ProdutosDAO {
             conn = new conectaDAO().connectDB();
 
             String sql = "INSERT into produtos(nome, valor) values (?,?)";
-            
+
             prep = conn.prepareStatement(sql);
             prep.setString(1, produto.getNome());
             prep.setInt(2, produto.getValor());
-            
+
             prep.execute();
             JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
-            
+
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Produto não foi cadastrado!" + e);
         }
     }
-    
+
     public ArrayList<ProdutosDTO> listarProdutos() {
+
+        String sql = "SELECT * FROM Produtos";
+        try {
+            conn = new conectaDAO().connectDB();
+
+            prep = conn.prepareStatement(sql);
+
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar os produtos do banco de dados" + e.getMessage());
+        }
 
         return listagem;
     }
+
+    public void venderProduto(Integer id) {
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+
+        try {
+            conn = new conectaDAO().connectDB();
+
+            prep = conn.prepareStatement(sql);
+            prep.setInt(1, id);
+            prep.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Produto vendido com sucesso!");
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Não foi possível realizar a venda do produto!" + e.getMessage());
+        }
+    }
+
 }
